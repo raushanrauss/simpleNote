08ae134063b15b2d52f370abc535a7d195dbabfa
Raushan kumar <john@example.com>
2024-07-30 03:50:40 +0530

    trying to fix error

--- Frontend/src/AllRoute/AllRoute.jsx
+++ Frontend/src/AllRoute/AllRoute.jsx
@@ -9,7 +9,7 @@ const AllRoute = () => {
   return (
     <div>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
               <Route path="/register" element={<Register />} />
               <Route path="/login" element={<Login />} />
           </Routes> 
